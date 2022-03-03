89c39ada4fcc9a26053fb6e12b53bfd54d1bb326
Domokos David <domokosdavid4@gmail.com>
2022-03-03 12:12:45 +0100

    4 feladat

--- src/Ugyfelkiszolgalo.java
+++ src/Ugyfelkiszolgalo.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.net.Socket;
 import java.nio.Buffer;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +15,51 @@ public class Ugyfelkiszolgalo implements  Runnable{
 
     @Override
     public void run() {
+        leghidegebbMa();
+        legmelegebbMa();
+        legmelegebbHolnap();
+        leghidegebbHolnap();
+    }
+
+    private String leghidegebbMa() {
+        Object elso = elorejelzesek.keySet().toArray()[0];
+        int minMa = elorejelzesek.get(elso).getMai().getMin();
+
+        for (Map.Entry<String, Idojaras> entry: elorejelzesek.entrySet()){
+            if(minMa > entry.getValue().getMai().getMin()){
+                minMa = entry.getValue().getMai().getMin();
+            }
+        }
+        return String.format("%d volt a leghidegebb a mai nap.", minMa);
+    }
+    private String legmelegebbMa() {
+        int maxMa = 0;
+        for (Map.Entry<String, Idojaras> entry: elorejelzesek.entrySet()){
+            if(maxMa < entry.getValue().getMai().getMax()){
+                maxMa = entry.getValue().getMai().getMax();
+            }
+        }
+        return String.format("%d volt a legmelegebb a mai nap.", maxMa);
+    }
+    private String legmelegebbHolnap() {
+        int maxHolnap = 0;
+        for (Map.Entry<String, Idojaras> entry: elorejelzesek.entrySet()){
+            if(maxHolnap < entry.getValue().getHolnapi().getMax()){
+                maxHolnap = entry.getValue().getHolnapi().getMax();
+            }
+        }
+        return String.format("%d lesz a legmelegebb holnap.", maxHolnap);
+    }
+    private String leghidegebbHolnap() {
+        Object elso = elorejelzesek.keySet().toArray()[0];
+        int minHolnap = elorejelzesek.get(elso).getHolnapi().getMin();
+
+        for (Map.Entry<String, Idojaras> entry: elorejelzesek.entrySet()){
+            if(minHolnap > entry.getValue().getHolnapi().getMin()){
+                minHolnap = entry.getValue().getHolnapi().getMin();
+            }
+        }
+        return String.format("%d lesz a leghidegebb holnap.", minHolnap);
 
     }
 
